69097a02e68690a263c94745e0fe7f17014e0e38
Jonathan Miller <jonathanmiller2@hotmail.com>
2020-04-05 00:55:36 -0500

    Login format completed

--- lib/main.dart
+++ lib/main.dart
@@ -28,72 +28,85 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Form(
-              child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.grey[400],
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.all(
-                        Radius.circular(5)
-                    )
-                ),
-
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          fillColor: Colors.white,
-                          hintText: 'Username',
-                        )
-                    ),
-                    Divider(
-                      color: Colors.grey[400],
-                      thickness: 1,
-                    ),
-                    TextField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        fillColor: Colors.blue,
-                        hintText: 'Password',
+      body: Container(
+        color: Colors.grey[200],
+        child: Padding(
+          padding: const EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Form(
+                child: Container(
+                  decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey[400],
+                        width: 1,
                       ),
-                    ),
-                  ],
+                      borderRadius: BorderRadius.all(
+                          Radius.circular(5)
+                      ),
+                      color: Colors.white,
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+                        child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              fillColor: Colors.white,
+                              hintText: 'Username',
+                            ),
+                        ),
+                      ),
+                      Container(
+                        color: Colors.grey,
+                        height: 1,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+                        child: TextField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            fillColor: Colors.blue,
+                            hintText: 'Password',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ),
 
-          Padding(
-            padding: const EdgeInsets.all(25),
-            child: FlatButton(
-                onPressed: (){},
-                color: Colors.green[700],
-                child: Text(
-                  'Sign In',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16
-                  ),
-                )
-            ),
-          )
-        ],
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 15),
+                child: FlatButton(
+                    onPressed: (){},
+                    color: Colors.green[700],
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18
+                      ),
+                    )
+                ),
+              )
+            ],
+          ),
+        ),
       ),
       bottomNavigationBar : BottomAppBar(
         shape: const CircularNotchedRectangle(),
-        child: Container(height: 50.0,),
+        child: Container(
+            height: 50.0
+        ),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: Icon(Icons.photo_camera),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,

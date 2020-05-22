21badfb8d082e8844b6e73d4d7dd467f8f3af591
Jonathan Miller <jonathanmiller2@hotmail.com>
2020-05-21 19:42:44 -0500

    Orientation test

--- lib/CameraScreen.dart
+++ lib/CameraScreen.dart
@@ -19,183 +19,187 @@ class _CameraScreenState extends State<CameraScreen> {
 	@override
 	Widget build(BuildContext context) {
 		
-		return Scaffold(
-			body: Column(
-				children: <Widget>[
-					Container(
-						color: Colors.black,
-						child: CameraPassthrough(),
+		return OrientationBuilder(
+			builder: (context, orientation) {
+				return Scaffold(
+					body: Column(
+						children: <Widget>[
+							Container(
+								color: Colors.black,
+								child: CameraPassthrough(),
+							),
+							Expanded(
+								child: Container(
+										color: Color.fromARGB(255, 20, 20, 20),
+										child: Center(
+												child: PositionIndicator()
+										)
+								),
+							),
+						],
 					),
-					Expanded(
+					bottomNavigationBar: BottomAppBar(
+						shape: const CircularNotchedRectangle(),
 						child: Container(
-								color: Color.fromARGB(255, 20, 20, 20),
-								child: Center(
-										child: PositionIndicator()
+								height: 50.0,
+								child: Row(
+									crossAxisAlignment: CrossAxisAlignment.stretch,
+									children: <Widget>[
+										Expanded(
+											child: Padding(
+												padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 0),
+												child: Align(
+														alignment: Alignment.centerLeft,
+														child: LabelledInvisibleButton(
+															label: "Close",
+															onPress: () {
+																Navigator.pop(context);
+															},
+															defaultColor: Colors.blue[600],
+															pressedColor: Colors.blue[200],
+															centered: false,
+															fontWeight: FontWeight.normal,
+														)
+												),
+											),
+										),
+										Expanded(
+											child: Padding(
+												padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 0),
+												child: Align(
+														alignment: Alignment.centerRight,
+														child: LabelledInvisibleButton(
+															label: "Geolock",
+															onPress: () {
+																if (!PositionIndicator.isGeolocked) {
+																	showDialog(
+																			context: context,
+																			builder: (BuildContext context) {
+																				return AlertDialog(
+																					title: Text("Enable geolock?"),
+																					content: Text("Enabling geolock saves the current latitude and longitude and saves them for future photos"),
+																					actions: <Widget>[
+																						FlatButton(
+																							child: Text("Cancel"),
+																							onPressed: () {
+																								Navigator.pop(context);
+																							},
+																						),
+																						FlatButton(
+																							child: Text("Enable"),
+																							onPressed: () {
+																								Navigator.pop(context);
+																								PositionIndicator.toggleGeolock();
+																								setState(() {});
+																							},
+																						)
+																					],
+																				
+																				);
+																			}
+																	
+																	);
+																}
+																else {
+																	PositionIndicator.toggleGeolock();
+																	setState(() {});
+																}
+															},
+															defaultColor: PositionIndicator.isGeolocked ? Colors.red[600] : Colors.blue[600],
+															pressedColor: PositionIndicator.isGeolocked ? Colors.red[200] : Colors.blue[200],
+															centered: false,
+															fontWeight: FontWeight.normal,
+														)
+												),
+											),
+										),
+									],
 								)
 						),
 					),
-				],
-			),
-			bottomNavigationBar: BottomAppBar(
-				shape: const CircularNotchedRectangle(),
-				child: Container(
-						height: 50.0,
-						child: Row(
-							crossAxisAlignment: CrossAxisAlignment.stretch,
-							children: <Widget>[
-								Expanded(
-									child: Padding(
-										padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 0),
-										child: Align(
-												alignment: Alignment.centerLeft,
-												child: LabelledInvisibleButton(
-													label: "Close",
-													onPress: () {
-														Navigator.pop(context);
-													},
-													defaultColor: Colors.blue[600],
-													pressedColor: Colors.blue[200],
-													centered: false,
-													fontWeight: FontWeight.normal,
-												)
-										),
-									),
-								),
-								Expanded(
-									child: Padding(
-										padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 0),
-										child: Align(
-												alignment: Alignment.centerRight,
-												child: LabelledInvisibleButton(
-													label: "Geolock",
-													onPress: () {
-														if(!PositionIndicator.isGeolocked) {
-															showDialog(
-																	context: context,
-																	builder: (BuildContext context) {
-																		return AlertDialog(
-																			title: Text("Enable geolock?"),
-																			content: Text("Enabling geolock saves the current latitude and longitude and saves them for future photos"),
-																			actions: <Widget>[
-																				FlatButton(
-																					child: Text("Cancel"),
-																					onPressed: () {
-																						Navigator.pop(context);
-																					},
-																				),
-																				FlatButton(
-																					child: Text("Enable"),
-																					onPressed: () {
-																						Navigator.pop(context);
-																						PositionIndicator.toggleGeolock();
-																						setState(() {});
-																					},
-																				)
-																			],
-																		
-																		);
-																	}
-															
-															);
-														}
-														else
-														{
-															PositionIndicator.toggleGeolock();
-															setState(() {});
-														}
-													},
-													defaultColor: PositionIndicator.isGeolocked ? Colors.red[600] : Colors.blue[600],
-													pressedColor: PositionIndicator.isGeolocked ? Colors.red[200] : Colors.blue[200],
-													centered: false,
-													fontWeight: FontWeight.normal,
-												)
-										),
-									),
-								),
-							],
-						)
-				),
-			),
-			floatingActionButton: FloatingActionButton(
-				backgroundColor: Colors.blue[600],
-				onPressed: () async {
-					
-					if(PositionIndicator.getMostRecentPosition() == null)
-					{
-						showDialog(
-								context: context,
-								builder: (BuildContext context) {
-									return AlertDialog(
-										title: Center(
-												child: Text(
-														"Error retrieving position"
-												)
-										),
-										content: Text(
-												"There was an error retrieving your position. This app requires your position to geotag photos for our database."
-										),
-										actions: <Widget>[
-											FlatButton(
-												child: Text("Dismiss"),
-												onPressed: () {
-													Navigator.pop(context);
-												},
-											),
-										],
-									);
-								}
-						);
-					}
-					else {
-						try {
-							DateTime timestamp = DateTime.now();
-							double latitude = PositionIndicator.getMostRecentPosition().latitude;
-							double longitude = PositionIndicator.getMostRecentPosition().longitude;
-							double heading = PositionIndicator.getMostRecentHeading().toDouble();
-							
-							final path = join(
-									(await getTemporaryDirectory()).path,
-									'${timestamp.year.toString()}-${timestamp.month.toString()}-${timestamp.day.toString()}-${timestamp.hour.toString()}-${timestamp.minute.toString()}-${timestamp.second.toString()}.jpg'
-							);
-							
-							await MyApp.cameraController.takePicture(path);
-							
-							//Data: Path, DateTime Taken, Latitude, Longitude, Heading, LULC Class, Field Notes
-							//List<Object> photoData = [path, timestamp, lat, long, heading, 0, ""];
-							
-							Navigator.push(
-								context,
-								new MaterialPageRoute(builder: (context) => new ImagePreviewScreen(imagePath: path, latitude: latitude, longitude: longitude, heading: heading,)),
-							);
-						} catch (e) {
-							print(e);
-							
-							showDialog(
-									context: context,
-									builder: (BuildContext context) {
-										return AlertDialog(
+					floatingActionButton: FloatingActionButton(
+						backgroundColor: Colors.blue[600],
+						onPressed: () async {
+							if (PositionIndicator.getMostRecentPosition() == null) {
+								showDialog(
+										context: context,
+										builder: (BuildContext context) {
+											return AlertDialog(
 												title: Center(
 														child: Text(
-																"Error taking photo"
+																"Error retrieving position"
 														)
 												),
 												content: Text(
-														"The image could not be taken. Error text:\n" + e.toString()
-												)
-										);
-									}
-							);
-						}
-					}
-				},
-				child: Icon(
-						Icons.camera,
-						size: 36
-				),
-			),
-			floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+														"There was an error retrieving your position. This app requires your position to geotag photos for our database."
+												),
+												actions: <Widget>[
+													FlatButton(
+														child: Text("Dismiss"),
+														onPressed: () {
+															Navigator.pop(context);
+														},
+													),
+												],
+											);
+										}
+								);
+							}
+							else {
+								try {
+									DateTime timestamp = DateTime.now();
+									double latitude = PositionIndicator
+											.getMostRecentPosition()
+											.latitude;
+									double longitude = PositionIndicator
+											.getMostRecentPosition()
+											.longitude;
+									double heading = PositionIndicator.getMostRecentHeading().toDouble();
+									
+									final path = join(
+											(await getTemporaryDirectory()).path,
+											'${timestamp.year.toString()}-${timestamp.month.toString()}-${timestamp.day.toString()}-${timestamp.hour.toString()}-${timestamp.minute.toString()}-${timestamp.second
+													.toString()}.jpg'
+									);
+									
+									await MyApp.cameraController.takePicture(path);
+									
+									//Data: Path, DateTime Taken, Latitude, Longitude, Heading, LULC Class, Field Notes
+									//List<Object> photoData = [path, timestamp, lat, long, heading, 0, ""];
+									
+									Navigator.push(
+										context,
+										new MaterialPageRoute(builder: (context) => new ImagePreviewScreen(imagePath: path, latitude: latitude, longitude: longitude, heading: heading,)),
+									);
+								} catch (e) {
+									print(e);
+									
+									showDialog(
+											context: context,
+											builder: (BuildContext context) {
+												return AlertDialog(
+														title: Center(
+																child: Text(
+																		"Error taking photo"
+																)
+														),
+														content: Text(
+																"The image could not be taken. Error text:\n" + e.toString()
+														)
+												);
+											}
+									);
+								}
+							}
+						},
+						child: Icon(
+								Icons.camera,
+								size: 36
+						),
+					),
+					floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+				);
+			}
 		);
-		
 	}
-	
 }
